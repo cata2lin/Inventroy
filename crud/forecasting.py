883144734ffd4ca68cfe6536d69c8e42cb43f777
cata2lin <carcucatalin@gmail.com>
2025-08-26 18:26:41 +0300

    fixxxx

--- crud/forecasting.py
+++ crud/forecasting.py
@@ -1,7 +1,7 @@
 # crud/forecasting.py
 
 from sqlalchemy.orm import Session, aliased
-from sqlalchemy import func, case, and_
+from sqlalchemy import func, case, and_, text
 from datetime import datetime, timedelta
 import models
 
