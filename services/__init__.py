3d10ca39a8047875c1435f059124ce2c25379647
cata2lin <carcucatalin@gmail.com>
2025-10-03 12:20:30 +0300

    asd

--- services/__init__.py
+++
@@ -1,7 +0,0 @@
-# services/__init__.py
-
-# This file makes the 'services' directory a Python package and
-# exposes its modules for import.
-
-from . import sync_tracker
-from . import product_sync_runner
