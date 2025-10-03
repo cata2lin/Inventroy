27cc6ff5b5f933864cccd46e22861e8244fe2e31
cata2lin <carcucatalin@gmail.com>
2025-10-03 12:15:19 +0300

    123

--- crud/__init__.py
+++
@@ -1,7 +0,0 @@
-# crud/__init__.py
-
-# By importing the modules here, we make them available to be imported directly
-# from the 'crud' package, like: from crud import store
-
-from . import store
-from . import product
